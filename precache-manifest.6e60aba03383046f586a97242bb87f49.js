2853aa754c39768b2cb55544f24ebca95b1d7dea
Anil Patnaik <anil.patnaik@ascenderhcm.com>
2020-06-06 11:31:54 +1200

    Updates

---
+++ precache-manifest.6e60aba03383046f586a97242bb87f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff0d2de3d2c34c9df6e6c1380490bc0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "143a0121a39dc0405708",
+    "url": "/todo/static/css/main.c338ef34.chunk.css"
+  },
+  {
+    "revision": "1bf7fac301b27bd9fe04",
+    "url": "/todo/static/js/2.de798808.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/todo/static/js/2.de798808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143a0121a39dc0405708",
+    "url": "/todo/static/js/main.1bbe5dca.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  }
+]);
